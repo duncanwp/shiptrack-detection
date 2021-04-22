00603479902c0d55a81eee31e8bc888238b7bfdd
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2021-04-22 16:25:15 +0100

    Adapting the composite for the best images and adding an overwrite option

--- prepare_training_data.py
+++ prepare_training_data.py
@@ -10,12 +10,13 @@ import argparse
 import pandas as pd
 from satpy import Scene
 from satpy.composites import GenericCompositor
+from satpy.dataset import DatasetID
 from satpy.writers import to_image
 from shapely.geometry import asLineString
-import PIL
+from PIL import Image
 
 
-def main(track_file, output_path, experiment_name, modis_path):
+def main(track_file, output_path, experiment_name, modis_path, overwrite=False):
     # Create output directories
     path_output_images = output_path + '/' + experiment_name + '/images/'
     os.makedirs(path_output_images, exist_ok=True)
@@ -27,7 +28,7 @@ def main(track_file, output_path, experiment_name, modis_path):
     os.makedirs(path_output_json, exist_ok=True)
 
     # Get labels
-    df = pd.read_csv(track_file, index_col='datetime', parse_dates=['datetime'])
+    df = pd.read_csv(track_file, index_col='datetime', parse_dates=['datetime']).sort_index()
     # Process each image
     for dt, image_tracks in df.groupby('datetime'):
 
@@ -37,27 +38,29 @@ def main(track_file, output_path, experiment_name, modis_path):
         img_id = mtype + dt.strftime("%Y%j.%H%M")
 
         # Now fetch MODIS file for selected instrument and time
-        filename = "{year/doy/*.A{year}{doy}.{hour}*.hdf".format(
-            year=dt.year, doy=dt.doy, hour=dt.time)
-        file02 = glob.glob(modis_path + mtype + "021km/" + filename)  # calibrated radiances
-        file03 = glob.glob(modis_path + mtype + "03/" + filename)  # geolocation
+        filename = "{year}/{month:02d}/{day:02d}/*.A???????.{hour}*.hdf".format(
+            year=dt.year, month=dt.month, day=dt.day, hour=dt.strftime("%H%M"))
+        file02 = glob.glob(modis_path + mtype.upper() + "021KM/collection61/" + filename)  # calibrated radiances
+        file03 = glob.glob(modis_path + mtype.upper() + "03/collection61/" + filename)  # geolocation
 
         # All MODIS L2 files must exist to proceed
         if (len(file02) > 0) and (len(file03) > 0):
 
             # Create image
             image_file = path_output_images + img_id + '.png'
-            if os.path.isfile(image_file):
+            if (not overwrite) and os.path.isfile(image_file):
                 print('exists: ' + image_file)
-                img = PIL.open(image_file)
+                img = Image.open(image_file) # Read the file anyway to get its size
             else:
                 global_scene = (Scene(reader="modis_l1b", filenames=file02 + file03))
-                global_scene.load(['1', '32'], resolution=1000)
-                global_scene.load([DatasetID(name='20', modifiers=('nir_emissive',))])
-                compositor = GenericCompositor("rgb")
-                composite = compositor([global_scene['1'], global_scene['32'], global_scene['20']])
-                img = to_image(composite)
-                img.stretch_hist_equalize("linear")
+                #global_scene.load(['1', '20', '32'], resolution=1000)
+                #global_scene.load([DatasetID(name='20', modifiers=('nir_reflectance',))])
+                #compositor = GenericCompositor("rgb")
+                #composite = compositor([global_scene['1'], global_scene['32'], global_scene['20']])
+                #img = to_image(composite)
+                global_scene.load(['day_microphysics']) # This uses channels 1, 20 and 31 (which is very close to the one above but looks much better)
+                img = to_image(global_scene['day_microphysics'])
+                img.stretch("histogram")
                 img.save(image_file)
 
             # Create points file
@@ -68,12 +71,11 @@ def main(track_file, output_path, experiment_name, modis_path):
             text_file.close()
 
             # Create JSON file for labelling
-            width, height = img.size  # Get the image size for the JSON
             json_file = open(path_output_json + img_id + '.json', "w")
             # TODO: I could probably do this in one line with geoPandas...
             shapes = []
             for _, track in image_tracks.groupby('track_id'):
-                points = asLineString(track['modis_cross', 'modis_along'].values)
+                points = asLineString(track[['modis_cross', 'modis_along']].values)
                 poly = points.buffer(5)
                 shapes.append({"label": "shiptrack",
                                "points": list(poly.exterior.coords),
@@ -83,10 +85,10 @@ def main(track_file, output_path, experiment_name, modis_path):
                                })
             json.dump({"version": "4.0.0", 'flags': {},
                        'shapes': shapes,
-                       "imagePath": image_file,
+                       "imagePath": "../images/" + img_id + '.png',  # Use the relative path
                        "imageData": None,
-                       "imageHeight": height,
-                       "imageWidth": width
+                       "imageHeight": img.height,
+                       "imageWidth": img.width
                        }, json_file)
         else:
             print('missing: ' + filename)
@@ -99,8 +101,9 @@ if __name__ == '__main__':
     parser.add_argument('track_file', metavar='track-file')
     parser.add_argument('output_path', metavar='output-path')
     parser.add_argument('experiment_name', metavar='experiment-name')
+    parser.add_argument('--overwrite', action='store_true')
     parser.add_argument('--modis-path', type=str,
-                        default='/group_workspaces/cems2/nceo_generic/satellite_data/modis_c61/')
+                        default='/neodc/modis/data/')
 
     args = parser.parse_args()
 
