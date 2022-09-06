fc607c7c28a723d3a34eca22a8b774fe504aa8bb
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2022-09-06 15:10:13 +0100

    Adding parallel processing and a nighttime flag

--- prepare_training_data.py
+++ prepare_training_data.py
@@ -8,13 +8,20 @@ import json
 import argparse
 
 import pandas as pd
+import numpy as np
+
+import satpy
 from satpy import Scene
 from satpy.writers import to_image
 from shapely.geometry import asLineString
 from PIL import Image
+from multiprocessing import Pool
+from functools import partial
+
+satpy.config.set(config_path=['/home/users/dwatsonparris/satpy_config'])
 
 
-def main(track_file, output_path, experiment_name, modis_path, overwrite=False):
+def main(track_file, output_path, experiment_name, modis_path, overwrite=False, n_processes=1, tar_output=False):
     # Create output directories
     path_output_images = output_path + '/' + experiment_name + '/images/'
     os.makedirs(path_output_images, exist_ok=True)
@@ -22,24 +29,55 @@ def main(track_file, output_path, experiment_name, modis_path, overwrite=False):
     path_output_points = output_path + '/' + experiment_name + '/points/'
     os.makedirs(path_output_points, exist_ok=True)
 
-    path_output_json = output_path + '/' + experiment_name + '/annotated/'
-    os.makedirs(path_output_json, exist_ok=True)
-
     # Get labels
     df = pd.read_csv(track_file, index_col='datetime', parse_dates=['datetime']).sort_index()
+
+    # Mask missing values
+    df = df.replace(-999, np.nan)
+
     # Process each image
-    for dt, image_tracks in df.groupby('datetime'):
-        try:
-            process_dt(dt, image_tracks, modis_path, overwrite,
-                       path_output_images, path_output_json, path_output_points)
-        except Exception as e:
-            print("Unable to process {}: {}".format(dt, e))
+    tasks = list(df.groupby('datetime'))
+    with Pool(n_processes) as p:
+        p.map(partial(process_safe, modis_path=modis_path, overwrite=overwrite,
+                      path_output_images=path_output_images,  
+                      path_output_points=path_output_points), tasks)
+
+    # tarball directory for easier downloading
+    if tar_output:
+        os.system("tar -czvf " + output_path + experiment_name + ".tar.gz" + " " + output_path + experiment_name + "/")
 
 
-def process_dt(dt, image_tracks, modis_path, overwrite, path_output_images, path_output_json, path_output_points):
+def is_daytime(dt, lat, lon):
+    from pyorbital import astronomy
+    #lons, lats = scene[0, 0].attrs["area"].get_lonlats() # Get the lon/lats for a single point in the corner
+    #angle = astronomy.sun_zenith_angle(scene.attrs["start_time"], lons[0], lats[0])
+    angle = astronomy.cos_zen(dt, lon, lat)
+    print(dt, lat, lon, angle)
+    return angle > 0
+
+def process_safe(grp, *args, **kwargs):
+    dt, image_tracks = grp
+    try:
+        process_dt(dt, image_tracks, *args, **kwargs)
+    except Exception as e:
+        print("Unable to process {}: {}".format(dt, e))
+
+
+def process_dt(dt, image_tracks, modis_path, overwrite, path_output_images, path_output_points):
     # Fetch corresponding MODIS type (they'll all be the same for this image)
     mtype = image_tracks.mtype[0]
-    img_id = mtype + dt.strftime("%Y%j.%H%M")
+    source = image_tracks.source[0]
+    
+    #day_str = image_tracks.daytime[0]
+    #daytime = day_str == 'D'
+
+    # Get the lat and lon from the first point after removing any NaNs
+    lat, lon = image_tracks.dropna().iloc[0][['lat', 'lon']].values
+
+    daytime = is_daytime(dt, lat, lon)
+    day_str = 'D' if daytime else 'N'
+
+    img_id = mtype + dt.strftime("%Y%j.%H%M") + day_str
     # Now fetch MODIS file for selected instrument and time
     filename = "{year}/{month:02d}/{day:02d}/*.A???????.{hour}*.hdf".format(
         year=dt.year, month=dt.month, day=dt.day, hour=dt.strftime("%H%M"))
@@ -55,24 +93,28 @@ def process_dt(dt, image_tracks, modis_path, overwrite, path_output_images, path
             img = Image.open(image_file)  # Read the file anyway to get its size
         else:
             global_scene = (Scene(reader="modis_l1b", filenames=file02 + file03))
-            global_scene.load(['day_microphysics'])  # This uses channels 1, 20 and 31)
-            img = to_image(global_scene['day_microphysics'])
+            composite = 'day_microphysics' if daytime else 'night_microphysics'
+            global_scene.load([composite], resolution=1000)  # This uses channels 1, 20 and 31)
+            img = to_image(global_scene[composite])
             img.stretch("histogram")
             img.save(image_file)
 
         # Create points file
         text_file = open(path_output_points + img_id + '.txt', "w")
         for _, track in image_tracks.groupby('track_id'):
-            track_points = track.modis_cross.astype(str) + " " + track.modis_along.astype('str')
+            # Sort the track points since the order matters
+            sorted_points = track.sort_values('point_id')
+            track_points = sorted_points.modis_cross.astype(str) + " " + sorted_points.modis_along.astype('str')
             text_file.write(track.source[0] + " " + " ".join(track_points) + "\n")
         text_file.close()
 
         # Create JSON file for labelling
-        json_file = open(path_output_json + img_id + '.json', "w")
+        json_file = open(path_output_images + img_id + '.json', "w")
         # TODO: I could probably do this in one line with geoPandas...
         shapes = []
         for _, track in image_tracks.groupby('track_id'):
-            points = asLineString(track[['modis_cross', 'modis_along']].values)
+            # Be sure to sort the track points first
+            points = asLineString(track.sort_values('point_id')[['modis_cross', 'modis_along']].values)
             poly = points.buffer(5)
             shapes.append({"label": "shiptrack",
                            "points": list(poly.exterior.coords),
@@ -80,9 +122,14 @@ def process_dt(dt, image_tracks, modis_path, overwrite, path_output_images, path
                            "shape_type": "polygon",
                            "flags": {}
                            })
-        json.dump({"version": "4.0.0", 'flags': {},
+        json.dump({"version": "4.0.0", 
+                   'flags': {'CSU': source=='CSU', 
+                             'OSU': source=='OSU',
+                             'SSI': source=='SSI',
+                             'KRL': source=='KRL',
+                             'EDG': source=='EDG'},
                    'shapes': shapes,
-                   "imagePath": "../images/" + img_id + '.png',  # Use the relative path
+                   "imagePath": img_id + '.png',  # Use the relative path
                    "imageData": None,
                    "imageHeight": img.height,
                    "imageWidth": img.width
@@ -99,6 +146,8 @@ if __name__ == '__main__':
     parser.add_argument('output_path', metavar='output-path')
     parser.add_argument('experiment_name', metavar='experiment-name')
     parser.add_argument('--overwrite', action='store_true')
+    parser.add_argument('--tar-output', action='store_true')
+    parser.add_argument('-n', '--n-processes', default=1, type=int)
     parser.add_argument('--modis-path', type=str,
                         default='/neodc/modis/data/')
 
@@ -106,6 +155,4 @@ if __name__ == '__main__':
 
     main(**vars(args))
 
-    # tarball directory for easier downloading
-    os.system("tar -czvf " + args.output_path + args.experiment_name + ".tar.gz" + " " + args.output_path +
-              args.experiment_name + "/")
+
